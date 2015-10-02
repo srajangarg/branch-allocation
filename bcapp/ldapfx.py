7e685da55eb9c3af274a1d98d19dc2845f196c94
Srajan Garg <srajan.garg@gmail.com>
2015-10-02 18:24:18 +0530

    minor

---
+++ bcapp/ldapfx.py
@@ -0,0 +1,22 @@
+import ldap
+
+def doLogin(userName, passWord):
+
+	userName = "uid="+userName
+	conn = ldap.initialize('ldap://ldap.iitb.ac.in')
+	search_result = conn.search_s('dc=iitb,dc=ac,dc=in', ldap.SCOPE_SUBTREE, userName, ['uid','employeeNumber'])
+
+	try:
+
+		if search_result:
+		    authenticate = conn.bind_s(search_result[0][0],passWord)
+		    # print search_result[0][1]['employeeNumber'][0] #roll
+		    # print search_result[0][1]['uid'][0] #username
+		    return(True, search_result[0][1]['employeeNumber'][0])
+		    
+		else:
+			return (False, "")
+
+	except ldap.INVALID_CREDENTIALS:
+	    return (False, "")
+
